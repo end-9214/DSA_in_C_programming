a9e04de134cca65c50eb50a333bfd91c233028a0
Karamveer Singh <karamveersingh2003111@Gmail.com>
2023-09-18 13:18:29 +0530

    binary search for sorted array

---
+++ 5_binary_search.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+int binarySearch(int arr[],int size,int element){
+  int mid,high,low;
+  low = 0;
+  high = size-1;
+  
+
+  while (low<=high){
+    mid = (low+high)/2;
+    if(arr[mid]==element){
+      return mid;
+    }
+    if(arr[mid]<element){
+      low = mid + 1;
+    }
+    else{
+      high = mid - 1;
+    }
+  }
+
+  return -1;
+
+}
+
+
+int main(){
+  //sorted array for binary search.
+
+  int arr[]={1,2,3,4,5,6,7,8,9,10};
+  int size = sizeof(arr)/sizeof(int);
+  int element;
+  printf("Enter the element to be searched: ");
+  scanf("%d",&element);
+  int searchIndex = binarySearch(arr,size,element);
+  printf("The element %d was found at index %d\n",element,searchIndex);
+
+
+  return 0;
+
+}
