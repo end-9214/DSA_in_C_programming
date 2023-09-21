5df217f16201145e45501263e22451312189ed49
Karamveer Singh <karamveersingh2003111@Gmail.com>
2023-09-21 16:33:12 +0530

    commit

--- 7_Linkedlists_insertion.c
+++ 7_Linkedlists_insertion.c
@@ -14,7 +14,7 @@ void linkedListTraversal(struct Node *ptr){
 
   }
 }
-
+// Case 1 
 struct Node * insertAtFirst(struct Node *head, int data){
   struct Node * ptr = (struct Node *)malloc(sizeof(struct Node));
   ptr->next = head;
@@ -23,16 +23,98 @@ struct Node * insertAtFirst(struct Node *head, int data){
 
 }
 
+// Case 2
+// insert at index
+
+struct Node * insertAtIndex(struct Node * head, int data, int index){
+  struct Node * ptr = (struct Node *)malloc(sizeof(struct Node));
+  struct Node * p = head;
+  int i = 0;
+
+
+  while(i!=index-1){
+    p = p->next;
+    i++;
+
+  };
+
+  ptr->data = data;
+  ptr->next = p->next;
+  p->next = ptr;
+ 
+  return head;
+
+
+
+
+
+}
+
+// Case 3
+// insertion at End
+struct Node * insertAtEnd(struct Node * head, int data){
+  struct Node * ptr = (struct Node *)malloc(sizeof(struct Node));
+   ptr->data = data;
+   struct Node *p = head;
+
+
+   while(p->next!=NULL){
+    p = p->next;
+
+
+   }
+
+   p->next = ptr;
+   ptr->next = NULL;
+   return head;
+   
+
+}
+
 int main(){
 
   struct Node * head;
 
   head = (struct Node *)malloc(sizeof(struct Node));
-   head -> data = 7;
-    head -> next = NULL;
+  struct Node * second;
+  second = (struct Node *)malloc(sizeof(struct Node));
+  struct Node * third;
+  third = (struct Node *)malloc(sizeof(struct Node));
+  struct Node * fourth;
+  fourth = (struct Node *)malloc(sizeof(struct Node));
+
+
+
+
+  head -> data = 7;
+  head -> next = second;
+
+  second -> data = 11;
+  second ->next= third;
+
+  third -> data= 66;
+  third -> next = fourth;
+
+  fourth -> data = 99;
+  fourth -> next = NULL;
+
 
   linkedListTraversal(head);
-  
+  printf("After insertion at first\n");
+  head = insertAtFirst(head,56);
+  linkedListTraversal(head);
+
+  printf("After insertion at index\n");
+  head = insertAtIndex(head,57,1);
+  linkedListTraversal(head);
+
+  printf("After insertion at End\n");
+  head = insertAtEnd(head,58);
+  linkedListTraversal(head);
+
+
+
+
 
 
 
