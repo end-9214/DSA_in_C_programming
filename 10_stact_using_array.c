e575c401262432cdccc6b87d3f8f3af2c2838754
Karamveer Singh <karamveersingh2003111@Gmail.com>
2023-09-25 11:17:02 +0530

    commit

--- 10_stact_using_array.c
+++ 10_stact_using_array.c
@@ -19,6 +19,15 @@ int isEmpty(struct stack * ptr){
     }
 }
 
+int isFull(struct stack * ptr){
+  if(ptr->top==ptr->size-1){
+    return 1;
+  }
+  else{
+    return 0;
+  }
+}
+
 int main(){
 
   // struct stack s;
@@ -28,9 +37,42 @@ int main(){
 
 
   struct stack *s;
-  s->size = 80;
+  s->size = 3;
   s->top = -1;
   s->arr = (int *)malloc(s->size * sizeof(int));
+
+  // push an element manually
+  // s->arr[0]=7;
+  // s->top++;
+
+  s->arr[0]=1;
+  s->top++;
+
+  s->arr[1]=2;
+  s->top++;
+
+  s->arr[2]=3;
+  s->top++;
+
+
+
+  // to check if stack is empty
+  if(isEmpty(s)){
+    printf("The stack is empty\n");
+
+  }
+  else{
+    printf("The stack is not empty\n");
+  }
+
+  if(isFull(s)){
+    printf("the stack if full\n");
+
+  }
+  else{
+    printf("the stack is not full\n");
+
+  }
 
   return 0;
 }
