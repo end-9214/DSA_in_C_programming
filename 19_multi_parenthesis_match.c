e8226412827f694ac9250c54aa985f23ef56dba1
Karamveer Singh <karamveersingh2003111@Gmail.com>
2023-09-28 19:00:58 +0530

    commit

--- 19_multi_parenthesis_match.c
+++ 19_multi_parenthesis_match.c
@@ -45,6 +45,28 @@ char pop(struct stack *ptr){
 
 int parenthesisMatch(char *exp){
   struct stack *sp;
+  sp->size = 100;
+  sp->top = -1;
+  sp->arr = (char *)malloc(sp->size * sizeof(char));
+
+  for (int i = 0; exp[i]!='\0'; i++)
+  {
+    if(exp[i]=='('){
+      push(sp, '(');
+    }
+    else if(exp[i]==')'){
+      if(isEmpty(sp)){
+        return 0;
+      }
+      pop(sp);
+    }
+  }
+  if(isEmpty(sp)){
+    return 1;
+  }
+  else{
+    return 0;
+  }
 
 }
 
