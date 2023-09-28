6451ec6fc666cc6fdae3eed9e7c011b12617847d
Karamveer Singh <karamveersingh2003111@Gmail.com>
2023-09-28 18:55:33 +0530

    commit

---
+++ 19_multi_parenthesis_match.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct stack{
+  int size;
+  int top;
+  char *arr;
+};
+
+int isEmpty(struct stack *ptr){
+  if(ptr->top == -1){
+    return 1;
+  }
+  return 0;
+}
+
+int isFull(struct stack *ptr){
+  if(ptr->top == ptr->size - 1){
+    return 1;
+  }
+  return 0;
+}
+
+void push(struct stack *ptr, char val){
+  if(isFull(ptr)){
+    printf("Stack Overflow\n");
+  }
+  else{
+    ptr->top++;
+    ptr->arr[ptr->top] = val;
+  }
+}
+
+char pop(struct stack *ptr){
+  if(isEmpty(ptr)){
+    printf("Stack Underflow\n");
+    return -1;
+  }
+  else{
+    char val = ptr->arr[ptr->top];
+    ptr->top--;
+    return val;
+  }
+}
+
+int parenthesisMatch(char *exp){
+  struct stack *sp;
+
+}
+
+int main(){
+  struct stack *s;
+  s->size = 100;
+  s->top = -1;
+  s->arr = (char *)malloc(sp->size * sizeof(char));
+
+  char *exp = "((8)(*--$$9))";
+  parenthesisMatch(exp);
+
+
+  return 0;
+}
