f5fa7e43f3d45a15f2d79a16633abd4629244d3a
Karamveer Singh <karamveersingh2003111@Gmail.com>
2023-10-07 10:24:24 +0530

    commit

--- 23_Queue_using_linkedlists.c
+++ 23_Queue_using_linkedlists.c
@@ -6,9 +6,37 @@ struct Node{
   struct Node *next;
 };
 
+void LinkedlistTraversal(struct Node *ptr){
+  printf("Printing the elements of this linked list\n");
+  while(ptr!=NULL){
+    printf("Element: %d\n",ptr->data);
+    ptr = ptr->next;
+
+  }
+}
+
+void enqueue(struct Node * f,struct Node *r, int val){
+  struct Node *n = (struct Node *)malloc(sizeof(struct Node));
+  if(n==NULL){
+    printf("Queue is full\n");
+
+  }else{
+    n->data = val;
+    n->next = NULL;
+    if(f==NULL){
+      f=r=NULL;
+    }else{
+      r->next = n;
+      r= n;
+    }
+  }
+
+}
+
 int main(){
   struct Node *f = NULL;
-  struct Node *r = NULL
+  struct Node *r = NULL;
+  LinkedlistTraversal(f);
 
 
   return 0;
