d76a85e59b120fe12f834c178f93596992c6734f
Karamveer Singh <karamveersingh2003111@Gmail.com>
2023-11-22 07:52:24 +0530

    quicksorting also done

--- 26_Quick_sort.c
+++ 26_Quick_sort.c
@@ -1,29 +1,68 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-void printArray(int *A, int n){
-  for (int i=0; i<=n; i++){
+void printArray(int *A, int n)
+{
+  for (int i = 0; i <= n; i++)
+  {
     printf("%d ", A[i]);
   }
   printf("\n");
 }
 
-void quickSort(int A[], int low, int high){
-  int partitionIndex; // Index of pivot after partition
+int partition(int A[], int low, int high)
+{
+  int pivot = A[low];
+  int i = low + 1;
+  int j = high;
+  int temp;
+  do
+  {
+    while (A[i] <= pivot)
+    {
+      i++;
+    }
 
-  partitionIndex = partition(A, low, high);
-  quickShort(A, low, partitionIndex-1); // Sort left subarray
-  quickSort(A, partitionIndex+1, high); // Sort right subarray
+    while (A[j] > pivot)
+    {
+      j--;
+    }
 
+    if (i < j)
+    {
+      temp = A[i];
+      A[i] = A[j];
+      A[j] = temp;
+    }
+  } while (i < j);
+
+  // Swap A[low] and A[j]
+   temp = A[low];
+   A[low] = A[j];
+   A[j] = temp;
+   return j;
 
 }
 
-int main(){
+void quickSort(int A[], int low, int high)
+{
+  int partitionIndex; // Index of pivot after partition
+
+  if (low < high)
+  {
+    partitionIndex = partition(A, low, high);
+    quickSort(A, low, partitionIndex - 1); // Sort left subarray
+    quickSort(A, partitionIndex + 1, high); // Sort right subarray
+  }
+}
+
+int main()
+{
   int A[] = {3, 5, 2, 1, 4};
-  int n=5;
+  int n = 5;
   printArray(A, n);
-  quickSort(A, 0, n-1);
+  quickSort(A, 0, n - 1);
   printArray(A, n);
-  
+
   return 0;
 }
