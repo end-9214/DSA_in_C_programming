fbc1de66887b318d190543ece835c915ec3604f2
Karamveer Singh <karamveersingh2003111@Gmail.com>
2023-12-10 14:54:43 +0530

    commit

--- 28_binary_tree.c
+++ 28_binary_tree.c
@@ -1,18 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct node{
+struct node
+{
   int data;
   struct node *left;
-  struct node * right;
+  struct node *right;
 };
 
-int main (){
-  struct node * p;
-  p = (struct node *)malloc(sizeof(struct node));
-  p->left = NULL;
-  p-> right = NULL;
+struct node *createNode(int data)
+{
+  struct node *n;
+  n = (struct node *)malloc(sizeof(struct node));
+  n->data = data;
+  n->left = NULL;
+  n->right = NULL;
+  return n;
+};
 
+int main()
+{
+  // struct node *p;
+  // p = (struct node *)malloc(sizeof(struct node));
+  // p->data = 2;
+  // p->left = NULL;
+  // p->right = NULL;
+
+  // struct node *p1;
+  // p1 = (struct node *)malloc(sizeof(struct node));
+  // p1->data = 1;
+  // p1->left = NULL;
+  // p1->right = NULL;
+
+  // struct node *p2;
+  // p2 = (struct node *)malloc(sizeof(struct node));
+  // p2->data = 3;
+  // p2->left = NULL;
+  // p2->right = NULL;
+  // // linking the root node with left and right children
+  // p->left = p1;
+  // p->right = p2;
+
+  struct node * p = createNode(2);
+  struct node * p1 = createNode(1);
+  struct node * p2 = createNode(3);
+
+  // linking the root node with left and right children
+  p->left = p1;
+  p->right = p2;
+  
 
   return 0;
 }
