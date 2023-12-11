989f96350c9ada7ebf81b3a161d8f868ffd330e3
Karamveer Singh <karamveersingh2003111@Gmail.com>
2023-12-11 11:54:18 +0530

    commit

---
+++ 30_preOrder_traversal.c
@@ -0,0 +1,52 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+  int data ;
+  struct node * left;
+  struct node * right;
+
+};
+
+struct node * createNode(int data){
+  struct node * n;
+  n = (struct node *)malloc(sizeof(struct node));
+  n->data = data;
+  n->left = NULL;
+  n->right = NULL;
+  return n;
+};
+
+void preOrder(struct node * root){
+  if(root!=NULL){
+    printf("%d ", root->data);
+    preOrder(root->left);
+    preOrder(root->right);
+  
+  }
+}
+
+int main(){
+
+  // Constructing the root node - Using Function (Recommended)
+  struct node *p = createNode(2);
+  struct node *p1 = createNode(4);
+  struct node *p2 = createNode(6);
+  struct node *p3 = createNode(5);
+  struct node *p4 = createNode(1);
+  
+
+
+  // Linking the root node with left and right children
+  p->left = p1;
+  p->right = p2;
+  p1->left = p3;
+  p1->right = p4;
+
+  
+
+  preOrder(p);
+  
+
+  return 0;
+}
