1f112e747f89f9c267f1eb94a7f757657c2ffef7
Karamveer Singh <karamveersingh2003111@Gmail.com>
2023-12-13 15:10:18 +0530

    commit

--- 32_binary_tree.c
+++ 32_binary_tree.c
@@ -16,22 +16,24 @@ struct node* createNode(int data) {
 }
 
 int main() {
-    struct node *root = createNode(1);
-    struct node *p1 = createNode(2);
-    struct node *p2 = createNode(3);
-    struct node *p3 = createNode(4);
-    struct node *p4 = createNode(5);
-    struct node *p5 = createNode(6);
-    struct node *p6 = createNode(7);
+    struct node *p = createNode(5);
+    struct node *p1 = createNode(3);
+    struct node *p2 = createNode(6);
+    struct node *p3 = createNode(1);
+    struct node *p4 = createNode(4);
 
-    root->left = p1;
-    root->right = p2;
+    /* Linking the nodes
+        5
+      /   \
+      3   6
+    /  \
+    1   4   */
+
+    p->left = p1;
+    p->right = p2;
 
     p1->left = p3;
     p1->right = p4;
-
-    p2->left = p5;
-    p2->right = p6;
 
     // Additional code for testing or further operations
 
