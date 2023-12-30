ff1bb29ef40e6e4b82c1fcbcaf2331caf4f87674
Karamveer Singh <karamveersingh2003111@Gmail.com>
2023-12-30 09:20:34 +0530

    commit

--- 36_Breadth_first_search_code.c
+++ 36_Breadth_first_search_code.c
@@ -1,44 +1,86 @@
 #include<stdio.h>
 #include<stdlib.h>
-
-struct circularqueue{
-  int size;
-  int f;
-  int r;
-  int *arr;
+ 
+struct queue
+{
+    int size;
+    int f;
+    int r;
+    int* arr;
 };
-
-void enqueue(struct circularqueue *q, int val){
-  if((q->r+1)%q->size==q->f){
-    printf("circularQueue is Full\n");
-  }else{
-    q->r=(q->r+1)%q->size;
-    q->arr[q->r]=val;
-  }
+ 
+ 
+int isEmpty(struct queue *q){
+    if(q->r==q->f){
+        return 1;
+    }
+    return 0;
 }
-
-void display(struct circularqueue *q){
-  for (int i=0;i<q->size;i++){
-    printf("%d\n",q->arr[i]);
-  }
+ 
+int isFull(struct queue *q){
+    if(q->r==q->size-1){
+        return 1;
+    }
+    return 0;
 }
-
+ 
+void enqueue(struct queue *q, int val){
+    if(isFull(q)){
+        printf("This Queue is full\n");
+    }
+    else{
+        q->r++;
+        q->arr[q->r] = val;
+        // printf("Enqued element: %d\n", val);
+    }
+}
+ 
+int dequeue(struct queue *q){
+    int a = -1;
+    if(isEmpty(q)){
+        printf("This Queue is empty\n");
+    }
+    else{
+        q->f++;
+        a = q->arr[q->f]; 
+    }
+    return a;
+}
+ 
 int main(){
-  struct circularqueue *q;
-  q->size = 4;
-  q->f = q->r = -1;
-  q->arr=(int *)malloc(q->size * sizeof(int));
-
-  enqueue(q,12);
-  enqueue(q,13);
-  enqueue(q,14);
-  enqueue(q,15);
-
-  display(q);
-
-  enqueue(q,16);
-
-  enqueue(q,17);
-
-  return 0;
+    // Initializing Queue (Array Implementation)
+    struct queue q;
+    q.size = 400;
+    q.f = q.r = 0;
+    q.arr = (int*) malloc(q.size*sizeof(int));
+    
+    // BFS Implementation 
+    int node;
+    int i = 0;
+    int visited[7] = {0,0,0,0,0,0,0};
+    int a [7][7] = {
+        {0,1,1,1,0,0,0},
+        {1,0,1,0,0,0,0},
+        {1,1,0,1,1,0,0},
+        {1,0,1,0,1,0,0},
+        {0,0,1,1,0,1,1},
+        {0,0,0,0,1,0,0}, 
+        {0,0,0,0,1,0,0} 
+    };
+    printf("%d ", i);
+    visited[i] = 1;
+    enqueue(&q, i); // Enqueue i for exploration
+    while (!isEmpty(&q))
+    {
+        int node = dequeue(&q);
+        for (int j = 0; j < 7; j++)
+        {
+            if(a[node][j] ==1 && visited[j] == 0){
+                printf("%d ", j);
+                visited[j] = 1;
+                enqueue(&q, j);
+            }
+        }
+    }
+    return 0;
 }
