df09fab73bb384adf74a63f25584851502c56ead
Karamveer Singh <karamveersingh2003111@Gmail.com>
2023-12-30 09:03:35 +0530

    commit

---
+++ 36_Breadth_first_search_code.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct circularqueue{
+  int size;
+  int f;
+  int r;
+  int *arr;
+};
+
+void enqueue(struct circularqueue *q, int val){
+  if((q->r+1)%q->size==q->f){
+    printf("circularQueue is Full\n");
+  }else{
+    q->r=(q->r+1)%q->size;
+    q->arr[q->r]=val;
+  }
+}
+
+void display(struct circularqueue *q){
+  for (int i=0;i<q->size;i++){
+    printf("%d\n",q->arr[i]);
+  }
+}
+
+int main(){
+  struct circularqueue *q;
+  q->size = 4;
+  q->f = q->r = -1;
+  q->arr=(int *)malloc(q->size * sizeof(int));
+
+  enqueue(q,12);
+  enqueue(q,13);
+  enqueue(q,14);
+  enqueue(q,15);
+
+  display(q);
+
+  enqueue(q,16);
+
+  enqueue(q,17);
+
+  return 0;
+}
